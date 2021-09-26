def5c62d152774cd557ba577e755cc95074b9f42
Anuj Yadav <91149204+yalrnr@users.noreply.github.com>
2021-09-26 15:05:14 +0530

    Problem_1_C_code

---
+++ Assignment1_prob1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{   
+       char number[100];
+       int length,i,DotCount=0,index;
+       scanf("%s",number);
+       length=strlen(number);
+       for(i=0;i<length;++i){
+           if(number[i]=='.'){
+               DotCount++;
+               index=i;
+           }
+       }
+       if(DotCount==1&&number[index+1]>=48&&number[index+1]<=57){
+       printf("Valid \n");
+       }
+       else{
+       printf("Invalid \n");
+       }
+    return 0;
+}
