820f8fdc1f2ed64bc433024a2414b7f79080b41b
Anuj Yadav <91149204+yalrnr@users.noreply.github.com>
2021-10-08 18:00:52 +0530

    Assignment1_prob4_C_soln

---
+++ Assignment1_prob4.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<time.h>
+void swap(int *x, int *y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+void selectionSort(int arr[], int n){
+    int i,j,key;
+    for(i=0;i<n-1;++i){
+        key=i;
+        for(j=i+1;j<n;++j)
+            if(arr[j]<arr[key])
+            key=j;
+            swap(&arr[key],&arr[i]);
+        }
+}
+void bubbleSort(int arr[], int n){
+    int i,j;
+    for(i=0;i<n-1;++i){    
+        for(j=0;j<n-i-1;++j){
+            if (arr[j]>arr[j+1])
+            swap(&arr[j], &arr[j+1]);
+            }
+        }
+}
+void calculation(int arr[],int n){
+    clock_t startSelectionSort,endSelectionSort,startBubbleSort,endBubbleSort;
+    long long int t_SelectionSort,t_BubbleSort;
+    startSelectionSort=clock();
+    selectionSort(arr, n);
+    endSelectionSort=clock();
+    startBubbleSort=clock();
+    bubbleSort(arr, n);
+    endBubbleSort=clock();
+    t_SelectionSort=endSelectionSort-startSelectionSort;
+    printf("Total elapsed time for Selection Sort : %lld\n", t_SelectionSort);
+    t_BubbleSort=endBubbleSort-startBubbleSort;
+    printf("Total elapsed time for Bubble Sort : %lld\n", t_BubbleSort);
+}
+int main(){
+    int n; scanf("%d",&n);
+    int arr[n],i,j; for(i=0;i<n;++i) scanf("%d",&arr[i]);
+    calculation(arr,n);
+    return 0;
+}
+/*
+Unsorted Array-
+Input-
+size=40 
+Output-
+Total elapsed time for Selection Sort : 6
+Total elapsed time for Bubble Sort : 4
+
+Input-
+size=1000
+Output-
+Total elapsed time for Selection Sort : 1791
+Total elapsed time for Bubble Sort : 1860
+
+Input-
+size=10,000
+Output-
+Total elapsed time for Selection Sort : 186630
+Total elapsed time for Bubble Sort : 202163
+
+*/
+/*
+Sorted Array-
+
+Input-
+size=40 
+Output-
+Total elapsed time for Selection Sort : 6
+Total elapsed time for Bubble Sort : 4
+
+Input-
+size=1000
+Output-
+Total elapsed time for Selection Sort : 1657
+Total elapsed time for Bubble Sort : 1771
+
+Input-
+size=10,000
+Output-
+Total elapsed time for Selection Sort : 175407
+Total elapsed time for Bubble Sort : 188659
+
+//The data is following a pattern , plot of their Time(for algorithms) vs Input data is  attached as a file.
+*/
+
